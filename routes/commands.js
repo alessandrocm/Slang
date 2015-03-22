7f9358337fbe880ad59f920526547f80288d11a2
Alessandro Catter-Martinez <alessandro.cattermartinez@gmail.com>
2015-03-21 22:12:17 -0400

    Integrated slack hook into command route.

--- routes/commands.js
+++ routes/commands.js
@@ -1,23 +1,36 @@
 var express = require('express');
-var SlackRequest = require('./../lib/slackRequest').SlackRequest;
+var slack = require('./../lib/slack');
+
+var opts = {
+  url : process.env.SLACK_HOOK
+};
 
 module.exports = function(bodyParser, commands){
   var router = express.Router();
   var urlencoder = bodyParser.urlencoded({extended:false});
 
   router.post('/commands', urlencoder, function(request,response) {
-    var slackRequest = new SlackRequest(request.body);
-    commands.run(slackRequest, function(error, result) {
+    var slackRequest = new slack.SlackRequest(request.body);
+    var hook = new slack.Hook(opts); 
+    var callback = function(error, result) {
       if (error) {
         response.sendStatus(500);
       }
-      else if (result.message) {
-        response.sendStatus(204); //TODO: Send message to Slack incoming hook.
+      else if (result && result.message) {
+        hook.send(result.message, function(err) {
+          if (err) {
+            response.sendStatus(500);
+          }
+          else {
+            response.sendStatus(204);
+          }
+        });    
       }
       else {
         response.sendStatus(204);
       }
-    });
+    };
+    commands.run(slackRequest,callback);
   });
   
   return router;
